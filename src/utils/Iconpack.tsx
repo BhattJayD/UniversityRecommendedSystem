3f6e55e7da268cecf2387360dceb22e2e686ff08
bhattjayd <cbhatt526@gmail.com>
2024-04-13 22:59:27 +0530

    icons added

--- src/utils/Iconpack.tsx
+++ src/utils/Iconpack.tsx
@@ -1,3 +1,9 @@
 export default {
   ICON: require('../assets/Icon.png'),
+  UK: require('../assets/UK.png'),
+  AUSTRALIA: require('../assets/dpaustralia2.png'),
+  CANADA: require('../assets/dpcanada2.png'),
+  NEWZEALAND: require('../assets/dpnewzealand2.png'),
+  USA: require('../assets/dpusa2.png'),
+  EU: require('../assets/EU.png'),
 };
